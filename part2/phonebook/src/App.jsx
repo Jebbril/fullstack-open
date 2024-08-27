9ffd1a8d34989444ca3cf94fdf8fa9a766fb183b
Omar Rakib <omarrakib.bis@gmail.com>
2024-08-27 13:50:09 +0100

    finished ex2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Persons = ({persons, filterValue}) => {
 	return (
 		persons.map(person => 
 			person.name.toLowerCase().includes(filterValue.toLowerCase())
-			? <p key={person.name}>{person.name} {person.number}</p>
+			? <p key={person.id}>{person.name} {person.number}</p>
 			: false )
 	)
 }
@@ -29,15 +30,24 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+
+	useEffect(() => {
+		console.log('effect')
+
+		axios.get("http://localhost:3001/persons")
+		.then(response => {
+			console.log('done')
+			setPersons(response.data)
+			// console.log(persons)
+		})
+		.catch(error => {
+      console.error('Error fetching data:', error)
+    })
+	},[])
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
